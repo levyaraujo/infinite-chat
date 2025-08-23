89a6384d4e4c91953a4df70a575f130b8354f374
Levy Araujo <lev0x79@gmail.com>
2025-08-23 20:23:13 -0300

    feat: ollama fly deploy and project files

---
+++ back/ollama_fly/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+juicefs format \
+    --storage s3 \
+    --bucket $AWS_ENDPOINT_URL_S3/$BUCKET_NAME \
+    --access-key $AWS_ACCESS_KEY_ID \
+    --secret-key $AWS_SECRET_ACCESS_KEY \
+    $DATABASE_URL \
+    juicefs-fly
+
+echo "Mounting JuiceFS to /root/.ollama"
+juicefs mount --prefetch=256 --buffer-size=12288 -d $DATABASE_URL /root/.ollama
+
+export OLLAMA_HOST=0.0.0.0:11434
+
+/bin/ollama serve
